f3b4d105843ca090db484addaaf6e8cf6aa90d33
StefanoR <rubinetti.stefano@gmail.com>
2025-03-17 10:38:50 +0100

    aggiunti esempi eccezioni

---
+++ src/eccezioni/Dimostrazione.java
@@ -0,0 +1,32 @@
+package eccezioni;
+
+import java.util.Scanner;
+
+public class Dimostrazione
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			chiediInputEDaiEccezione();
+		}
+		catch (RuntimeException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+
+	static void chiediInputEDaiEccezione() throws InputSbagliatoException,RuntimeException
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("SE metti pippo do runtime,con paperino InputSbagliato");
+		String in = sc.nextLine();
+
+		if(in.equalsIgnoreCase("pippo"))
+			throw new RuntimeException("HAI MESSO PIPPO");
+
+		if(in.equalsIgnoreCase("paperino"))
+			throw new InputSbagliatoException("HAI MESSO PAPERINO","INPUT STRINGA");
+	}
+}
---
+++ src/eccezioni/InputSbagliatoException.java
@@ -0,0 +1,30 @@
+package eccezioni;
+
+public class InputSbagliatoException extends RuntimeException
+{
+	private String tipologiaInput;
+
+	public InputSbagliatoException(String messaggio)
+	{
+		super(messaggio);
+		//qui sto RICHIAMANDO il COSTRUTTORE DEL MIO SUPERTIPO
+		//SUPERTIPO/SUPERCLASSE -> CLASSE DI CUI FACCIAMO extends
+	}
+
+	public InputSbagliatoException(String messaggio,String tipologiaInput)
+	{
+		super(messaggio);
+		this.tipologiaInput = tipologiaInput;
+	}
+
+	public void insultaUtente()
+	{
+		System.out.println("BUFFONE,INSERISCI DATI BENE");
+	}
+
+
+	public String produciMessaggioBello()
+	{
+		return "Errore derivato da "+tipologiaInput+"| messaggio: "+getMessage();
+	}
+}
